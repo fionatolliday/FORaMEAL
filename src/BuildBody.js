ce113d35cf93fc0d4a55f886fa7414d7a2dd3854
Fiona Tolliday <fionatolliday@hotmail.com>
2018-12-15 19:52:26 +1100

    body and what we do

--- src/BuildBody.js
+++ src/BuildBody.js
@@ -8,6 +8,8 @@ const BuildBody = () => {
         <div className='container'>
 
             <span className='driving-force'>
+                <p>Last year alone, <strong> 4,987 people </strong> were killed and <strong> 34.5 million </strong> were affected by floods, storms and extreme temperatures. </p>
+                
                 <p>The driving force behind the FORaMEAL project
                 is to provide food and develop strong networks in neighbouring countries so that we
                 can share our abundant resources to aid when disasters occur.</p>
