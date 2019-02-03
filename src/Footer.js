d5d9e521ee1ca756c83a80d038e507341274905b
Fiona Tolliday <fionatolliday@hotmail.com>
2019-02-03 15:21:31 +1100

    meetup logo

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import logo from './assets/logo-rotarycanterbury.png'
+import meetuplogo from './assets/meetup.svg'
 
 const Footer = () => {
     return (
             <footer>
                     <a href="https://www.canterburyrotary.org">
                         <img src={logo} alt="Rotary logo" />
+                    </a>
+
+                    <a href="https://www.meetup.com/en-AU/FORaMEAL-Emergency-Food-Relief-Packing-Volunteers/">
+                        <img src={meetuplogo} id="meetup" alt="Meetup" />
                     </a>
             </footer>
 
