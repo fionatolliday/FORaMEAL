8c5b012df57d83b1b59cd4b7e914667a4970edc6
Fiona Tolliday <fionatolliday@hotmail.com>
2019-06-16 08:31:05 +1000

    FB logo rather than link

--- src/Footer.js
+++ src/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './assets/logo-rotarycanterbury.png'
 import meetuplogo from './assets/meetup.svg'
+import facebooklogo from './assets/f_logo_RGB-Blue_58.png'
 
 const Footer = () => {
     return (
@@ -14,7 +15,7 @@ const Footer = () => {
                     </a>
                     
                     < a href="https://www.facebook.com/FORaMEAL/">
-                    <i class="fab fa-facebook-square"></i>
+                    <img src={facebooklogo} alt="Facebook logo" />
                     </a>
             </footer>
 
